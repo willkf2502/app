0e76bcaa1e725b765e390d0e6f5f4d9b787cb46e
Willian <Willian@inffoc.com.br>
2022-09-13 21:19:26 -0300

    JointJS Instance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { MenuComponent } from './components/menu/menu.component';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { MaintabsComponent } from './components/maintabs/maintabs.component';
 import { DrawPapperComponent } from './components/draw-papper/draw-papper.component';
+import { DrawToolbarComponent } from './components/draw-toolbar/draw-toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     MaintabsComponent,
-    DrawPapperComponent
+    DrawPapperComponent,
+    DrawToolbarComponent
   ],
   imports: [
     BrowserModule,
